c7daf75a0b921ea05d7ca3f8f3137876eee5c673
Akash Saraf <akashsaraf24@gmail.com>
2025-09-02 12:47:48 +0530

    Lazy init and getBean by class

--- src/main/java/org/akash/App.java
+++ src/main/java/org/akash/App.java
@@ -12,7 +12,7 @@ public class App
     public static void main( String[] args )
     {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-        Alien obj = (Alien) context.getBean("alien");
+        Alien obj = context.getBean("alien", Alien.class);
         obj.code();
         System.out.println(obj.getAge());
     }
