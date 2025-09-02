dc70f0c7fcc7bc68f4af62f862eca33c0d305cb9
Akash Saraf <akashsaraf24@gmail.com>
2025-09-02 12:23:31 +0530

    Constructor Injection

--- src/main/java/org/akash/Alien.java
+++ src/main/java/org/akash/Alien.java
@@ -2,7 +2,16 @@ package org.akash;
 
 public class Alien {
     private int age;
+    private Laptop lap;
 
+    public Alien(int age, Laptop lap) {
+        this.age = age;
+        this.lap = lap;
+    }
+
+    public Alien(){
+        System.out.println("Alien obj created");
+    }
     public int getAge() {
         return age;
     }
@@ -11,10 +20,16 @@ public class Alien {
         this.age = age;
     }
 
-    public Alien(){
-        System.out.println("Alien obj created");
+    public Laptop getLap() {
+        return lap;
     }
+
+    public void setLap(Laptop lap) {
+        this.lap = lap;
+    }
+
     public void code(){
         System.out.println("Coding");
+        lap.compile();
     }
 }
